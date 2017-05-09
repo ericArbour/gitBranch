6bdfb006922b6bb32c42987558b191540d1ca34b
Eric Arbour <arboureric@gmail.com>
2017-05-09 13:43:09 -0400

    palindrome functionality added

---
+++ script.js
@@ -0,0 +1,25 @@
+for(var i = 0; i < 30; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log('fizzbuzz');
+  } else if (i % 5 === 0) {
+    console.log('buzz');
+  } else if (i % 3 === 0) {
+    console.log('fizz');
+  } else {
+    console.log(i);
+  }
+}
+
+function isPal (word) {
+  var ogWord = word;
+  if (ogWord === word.split('').reverse().join()) {
+    console.log('ya it is');
+  } else {
+    console.log('no');
+  }
+}
+
+isPal('mom');
+isPal('taco');
+isPal('spaghetti');
+isPal('tacocat');
